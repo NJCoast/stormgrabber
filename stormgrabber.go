440e0f715e8dc8c9a9b5fc8dabc3485daf925ed5
Charles F Vardeman II <charles.vardeman@gmail.com>
2018-07-24 11:59:36 -0400

    Working file path command line

--- stormgrabber.go
+++ stormgrabber.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/mmcdole/gofeed"
 )
 
 func main() {
+	var pathslice []string
+	var fullpath string
+
+	wordPtr := flag.String("dir", ".", "KMZ file download directory")
+	flag.Parse()
+	downloadDir := *wordPtr
+	fmt.Println(downloadDir)
 
 	fp := gofeed.NewParser()
 	feed, _ := fp.ParseURL("https://www.nhc.noaa.gov/rss_examples/gis-at.xml")
@@ -20,8 +30,15 @@ func main() {
 		if strings.Contains(item.Title, "Preliminary Best Track Points [kmz]") {
 			fmt.Println(item.Title)
 			fmt.Println(item.Link)
-
-			err := DownloadFile("test.kmz", item.Link)
+			//Use url parse and path to get the filename
+			u, err := url.Parse(item.Link)
+			if err != nil {
+				panic(err)
+			}
+			filename := path.Base(u.RequestURI())
+			pathslice = append(pathslice, downloadDir, "/", filename)
+			fullpath = strings.Join(pathslice, "")
+			err = DownloadFile(fullpath, item.Link)
 			if err != nil {
 				panic(err)
 			}
@@ -30,6 +47,8 @@ func main() {
 
 }
 
+//DownloadFile is a file downloader utility function
+// https://golangcode.com/download-a-file-from-a-url/
 func DownloadFile(filepath string, url string) error {
 
 	// Create the file
